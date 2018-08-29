a88362beeefdf4e71b06b3a0d02b571fd4b83f6f
wsxingjun <1185889308@qq.com>
2018-08-29 14:44:30 +0800

    v5-相册选择照片并剪裁显示

--- app/src/main/java/com/codingmankk/www/androidcamera/MainActivity.java
+++ app/src/main/java/com/codingmankk/www/androidcamera/MainActivity.java
@@ -1,16 +1,23 @@
 package com.codingmankk.www.androidcamera;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -38,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
     private static final int RESUTL_CAMERA_ONLY = 100;
     private static final int RESULT_CAMERA_CROP_PATH_RESULT = 301;
     private static final int RESULT_ALBUM_ONLY_THROUGH_RESULT = 302; //相册选取照片-缩略图
+    private static final int RESULT_ALBUM_CORP_URI_RESULT = 303; //相册选取照片-剪裁-缩略图
+    private static final int RESULT_ALBUM_CORP_PATH_RESULT = 401; //相册选取照片-剪裁-缩略图
+    private static final int RESULT_ALBUM_CORP_PATH = 400; //相册选取照片-剪裁-缩略图
     /**
      * 存放拍照结果
      */
@@ -51,17 +61,16 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.Btn_Capture)
     Button mBtnCapture;
 
-    @BindView(R.id.Btn_SelectImageResSmall)
-    Button mBtnSelectImageResSmall;
+    @BindView(R.id.Btn_Select_Album_Crop_Uri)
+    Button mBtnSelect_Album_Crop_Uri;
 
-
+    @BindView(R.id.Btn_Select_Album_Crop_Path)
+    Button mSelect_Album_Crop_Path;
 
 
     @BindView(R.id.IV_ImageView)
     ImageView mIV;
-
-
-
+    private File tempFile;
 
 
     @Override
@@ -89,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private String getAbsPath(){
+    private String getAbsPath() {
         return Environment.getExternalStorageDirectory().getAbsolutePath();
     }
 
@@ -112,10 +121,43 @@ public class MainActivity extends AppCompatActivity {
      * [2]相册选取照片-缩略图
      */
     @OnClick(R.id.Btn_SelectImageResSmall)
-    public void mBtnSelectImageResSmall(){
+    public void mBtnSelectImageResSmall() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
         intent.setType("image/*");
-        startActivityForResult(intent,RESULT_ALBUM_ONLY_THROUGH_RESULT);
+        startActivityForResult(intent, RESULT_ALBUM_ONLY_THROUGH_RESULT);
+    }
+
+    /**
+     * [3]相册选取照片-剪裁-缩略图-uri--显示不出来图片!!!!!失败
+     */
+    @OnClick(R.id.Btn_Select_Album_Crop_Uri)
+    public void mBtnSelectAlbumCrop() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
+        intent.setType("image/*");
+        intent.putExtra("crop", true);
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("outputX", 1000);
+        intent.putExtra("outputY", 1000);
+        intent.putExtra("scale", true);
+        intent.putExtra("return-data", false);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageCropUri);
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        intent.putExtra("noFaceDetection", true);
+        startActivityForResult(intent, RESULT_ALBUM_CORP_URI_RESULT);
+    }
+
+    /**
+     * [4] 相册选取照片-剪裁-缩略图-path
+     */
+    @OnClick(R.id.Btn_Select_Album_Crop_Path)
+    public void BtnSelectAlbumCropPath() {
+        /**
+         * 启动相册
+         */
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
+        intent.setType("image/*");
+        startActivityForResult(intent, RESULT_ALBUM_CORP_PATH);
     }
 
 
@@ -151,12 +193,42 @@ public class MainActivity extends AppCompatActivity {
             case RESULT_ALBUM_ONLY_THROUGH_RESULT: //相册选取照片-缩略图
                 try {
                     Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
-                    if (bitmap != null){
-                        Bitmap bitmap1 = setScaleBitmap(bitmap,2);
+                    if (bitmap != null) {
+                        Bitmap bitmap1 = setScaleBitmap(bitmap, 2);
                         mIV.setImageBitmap(bitmap1);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
+                }
+                break;
+
+            case RESULT_ALBUM_CORP_URI_RESULT:
+                try {
+                    Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(mImageUri));
+                    if (bitmap != null) {
+                        Bitmap newBitmap = setScaleBitmap(bitmap, 2);
+                        mIV.setImageBitmap(newBitmap);
+                    }
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case RESULT_ALBUM_CORP_PATH:
+                String path = parsePicPath(MainActivity.this, data.getData());
+                File file = new File(path);
+                Uri uri = Uri.fromFile(file);
+                cropImgPath(uri);
+                break;
+            case RESULT_ALBUM_CORP_PATH_RESULT:
+                Bundle extra = data.getExtras();
+                if (extra != null){
+                    Bitmap bitmap = BitmapFactory.decodeFile(getTempFile().getAbsolutePath(), null);
+                    if (bitmap != null){
+                        Bitmap scaleBitmap = setScaleBitmap(bitmap, 2);
+                        mIV.setImageBitmap(scaleBitmap);
+                    }
                 }
                 break;
             default:
@@ -166,19 +238,132 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void cropImgPath(Uri uri) {
+        File tempFile = getTempFile();
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", true);
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("outputX", 700);
+        intent.putExtra("outputY", 700);
+        intent.putExtra("scale", true);
+        intent.putExtra("return-data", false);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        intent.putExtra("noFaceDetection", true);
+        startActivityForResult(intent, RESULT_CAMERA_CROP_PATH_RESULT);
+    }
+
+    @SuppressLint("NewApi")
+    private String parsePicPath(Context context, Uri uri) {
+        if (context == null || uri == null) {
+            return null;
+        }
+
+        boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+
+            if (isExternalStorageDocumentsUri(uri)) {
+
+                String docId = DocumentsContract.getDocumentId(uri);
+                String[] splits = docId.split(":");
+                String type = splits[0];
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + File.separator + splits[1];
+                }
+            } else if (isDownloadsDocumentsUri(uri)) {
+                String docId = DocumentsContract.getDocumentId(uri);
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                return getDataColumn(context, contentUri, null, null);
+            }
+
+            // MediaDocumentsUri
+            else if (isMediaDocumentsUri(uri)) {
+                String docId = DocumentsContract.getDocumentId(uri);
+                String[] split = docId.split(":");
+                String type = split[0];
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                String selection = "_id=?";
+                String[] selectionArgs = new String[]{split[1]};
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            if (isGooglePhotosContentUri(uri))
+                return uri.getLastPathSegment();
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+
+
+    }
+
+    private static boolean isExternalStorageDocumentsUri(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isDownloadsDocumentsUri(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isMediaDocumentsUri(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    private static boolean isGooglePhotosContentUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+    private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+
+        Cursor cursor = null;
+        String column = "_data";
+        String[] projection = {column};
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            try {
+                if (cursor != null)
+                    cursor.close();
+            } catch (Exception e) {
+                Log.e("ozTaking",e.getMessage());
+            }
+        }
+        return null;
+
+    }
+
     private Bitmap setScaleBitmap(Bitmap bitmap, int scale) {
 
-        if (bitmap != null){
+        if (bitmap != null) {
             int sourceWidth = bitmap.getWidth();
             int sourceHeight = bitmap.getHeight();
             Matrix matrix = new Matrix();
-            int cropWidth = sourceWidth/scale;
-            int cropHeight = sourceHeight/scale;
+            int cropWidth = sourceWidth / scale;
+            int cropHeight = sourceHeight / scale;
 
-            float scaleWidth = (float) cropWidth /sourceWidth;
-            float scaleHeight = (float) cropHeight /sourceHeight;
+            float scaleWidth = (float) cropWidth / sourceWidth;
+            float scaleHeight = (float) cropHeight / sourceHeight;
 
-            matrix.postScale(scaleWidth,scaleHeight);
+            matrix.postScale(scaleWidth, scaleHeight);
             Bitmap newBitmap = Bitmap.createBitmap(bitmap, 0, 0, sourceWidth, sourceHeight, matrix, true);
             /**
              * 释放原始图片占用的内存，防止out of memory异常发生
@@ -263,5 +448,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return Environment.getExternalStorageDirectory().getPath();
+    }
+
+    public File getTempFile() {
+
+        try {
+            return new File(getSDCardPath()+"/temp_crop.jpg");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
     }
 }
