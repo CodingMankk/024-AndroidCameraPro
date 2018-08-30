38f422212c5f8134b7644ac8a7c2f19009417fe1
wsxingjun <1185889308@qq.com>
2018-08-30 23:13:03 +0800

    temp save

--- CH1CameraRecordVideo/src/main/java/com/oztaking/www/ch1camerarecordvideo/CustomCameraRecordVideoActivity.java
+++ CH1CameraRecordVideo/src/main/java/com/oztaking/www/ch1camerarecordvideo/CustomCameraRecordVideoActivity.java
@@ -5,9 +5,12 @@ import android.content.res.Configuration;
 import android.hardware.Camera;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.Chronometer;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -15,6 +18,8 @@ import com.orhanobut.logger.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -41,13 +46,18 @@ public class CustomCameraRecordVideoActivity extends Activity{
      * UI
      */
     @BindView(R.id.IV_Record_control)
-    ImageView mIV;
+    ImageView mIVStart;
 
-//    @BindView(R.id.Btn_RestartVideo)
-//    Button mBtnRestartVideo;
+    @BindView(R.id.IV_Record_Pause)
+    ImageView mIVPause;
+
+
 
     @BindView(R.id.id_record_surfaceView)
     SurfaceView mSurfaceView;
+
+    @BindView(R.id.chronometer_recordTime)
+    Chronometer mChronometerRecordTime;
 
     private SurfaceHolder mSurfaceViewHolder;
 
@@ -67,7 +77,7 @@ public class CustomCameraRecordVideoActivity extends Activity{
             }
         }
     };
-
+    private File mRecordFile1;
 
 
     @Override
@@ -190,13 +200,105 @@ public class CustomCameraRecordVideoActivity extends Activity{
 
 
     @OnClick(R.id.IV_Record_control)
-    public void StartRecordVideo(){
+    public void StartOrPauseRecordVideo(){
         if (!isRecording){
             //Todo 开始录制视屏
+            startRecordVideo();
         }else{
             //Todo 停止视频录制
+            stopRecordVideo();
         }
     }
+
+    /**
+     * 开始录制视频
+     */
+    private void startRecordVideo() {
+        boolean isCreateOK = createRecordDir();
+        if (!isCreateOK){
+            return;
+        }
+
+        initCamera();
+        mCamera.unlock();
+        setConfigRecord();
+
+        try {
+            //开始录制
+            mMediaRecorder.prepare();
+            mMediaRecorder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        isRecording = true;
+        if (mRecordCurrentTime != 0){
+            mChronometerRecordTime.setBase(
+                    SystemClock.elapsedRealtime()-(mRecordCurrentTime - mChronometerRecordTime.getBase()));
+        }else{
+            mChronometerRecordTime.setBase(SystemClock.elapsedRealtime());
+        }
+
+        mChronometerRecordTime.start();
+
+    }
+
+    private void setConfigRecord() {
+
+    }
+
+
+    /**
+     * 结束录制视频
+     */
+    private void stopRecordVideo() {
+        if (isRecording && mMediaRecorder != null){
+            //防止出现错误之后崩溃
+            mMediaRecorder.setOnErrorListener(null);
+            mMediaRecorder.setPreviewDisplay(null);
+            //停止录制
+            mMediaRecorder.stop();
+            mMediaRecorder.reset();
+            //释放资源
+            mMediaRecorder.release();
+            mMediaRecorder = null;
+
+            mChronometerRecordTime.stop();
+            //设置开始按钮可点击，停止按钮不可点击
+            mIVStart.setEnabled(true);
+            mIVPause.setEnabled(false);
+            isRecording = false;
+        }
+    }
+
+
+    private boolean createRecordDir() {
+
+        if (!Environment.MEDIA_MOUNTED.equals(
+                Environment.getExternalStorageState())){
+            Toast.makeText(this, "sd卡错误", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        /**
+         *保存的文件路径
+         */
+        File recordFileDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM),"Record");
+        if (!recordFileDir.exists()){
+            recordFileDir.mkdirs();
+        }
+
+        /**
+         * 保存的文件名称
+         */
+        String recordFileName = "VID"+new SimpleDateFormat("yyyyMMdd_HHmmss")
+                .format(new Date())+".mp4";
+        mRecordFile = new File(recordFileDir, recordFileName);
+
+        return true;
+    }
+
+
 
 
 }
